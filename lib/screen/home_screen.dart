342c9cee28ce988891cffa15e9908cb9d57e29ab
Haruachm <horang_e7@naver.com>
2023-02-08 17:21:05 +0900

    StatelessWidget ===> StatefulWidget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final Color color;
 
   const HomeScreen({
@@ -9,11 +9,16 @@ class HomeScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: 150.0,
       height: 150.0,
-      color: color,
+      color: widget.color,
     );
   }
 }
